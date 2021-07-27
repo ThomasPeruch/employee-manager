865d030830348d3a89dbd0962eb45af082817aac
ThomasPeruch <peruchthomas@gmail.com>
2021-07-27 17:27:36 -0300

    Employee repository

---
+++ backEnd/employee-manager/src/main/java/com/tperuch/employeemanager/repository/EmployeeRepository.java
@@ -0,0 +1,6 @@
+package com.tperuch.employeemanager.repository;
+
+import com.tperuch.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {}
