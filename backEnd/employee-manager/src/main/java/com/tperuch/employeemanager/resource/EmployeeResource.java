534a73d12c7bedb911da9ac83fcd386d84336f9e
ThomasPeruch <peruchthomas@gmail.com>
2021-08-03 09:18:37 -0300

    getEmployeeById in controller

--- backEnd/employee-manager/src/main/java/com/tperuch/employeemanager/resource/EmployeeResource.java
+++ backEnd/employee-manager/src/main/java/com/tperuch/employeemanager/resource/EmployeeResource.java
@@ -5,6 +5,7 @@ import com.tperuch.employeemanager.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +26,11 @@ public class EmployeeResource {
         List<Employee> employees = service.findAllEmployees();
         return new ResponseEntity<>(employees, HttpStatus.OK);
     }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id){
+        Employee employee = service.findEmployeeById(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
 }
